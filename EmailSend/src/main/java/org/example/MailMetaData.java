7aa311b562bda844ba4540eca8f9f58d0f3fe2ca
Biswajeet Mohanta <biswajeetmohanta123@gmail.com>
2023-06-11 21:56:04 +0530

    maven project send mail using java

---
+++ EmailSend/src/main/java/org/example/MailMetaData.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class MailMetaData {
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String port = "465";
+    public static final String sslProperty = "mail.smtp.ssl.enable";
+    public static final String authPerm = "mail.smtp.auth";
+
+    public static final String myUserMail = "biswajeetmohanta123@gmail.com";
+
+    public static final String password = "uxgblaqoncullxjt";// app password
+
+    public static final String receiverMail = "rb486373@gmail.com";
+
+}
